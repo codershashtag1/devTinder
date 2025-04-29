baf430732e41b84f4389bf9cf738ad845aae60be
codershashtag1 <darshanapandey123@gmail.com>
2025-04-29 10:07:01 +0530

    Update chron Time

--- src/utils/cron.js
+++ src/utils/cron.js
@@ -3,7 +3,7 @@ const ConnectionRequestModel = require('../models/connectionRequestModel');
 const { subDays, startOfDay, endOfDay } = require('date-fns')
 const ses_sendEmail = require('./ses_sendEmail');
 
-cron.schedule('0 8 * * * *', async () => {
+cron.schedule('0 8 * * *', async () => {
   try {
     
     let yesterday = subDays(new Date(), 1);
@@ -21,7 +21,7 @@ cron.schedule('0 8 * * * *', async () => {
     let listOfEmails = [...new Set(pendingRequest.map(e => e.toUserId.email))]
     console.log(listOfEmails);
 
-    let data = await ses_sendEmail.run(`Please Login to devTinder and check the requests you have received`)
+  //  / let data = await ses_sendEmail.run(`Please Login to devTinder and check the requests you have received`)
 
     // for(let i = 0; i < listOfEmails.length; i++) {
     //   let data = await ses_sendEmail.run(`Please Login to devTinder and check the requests you have received ${listOfEmails[i]}`)
